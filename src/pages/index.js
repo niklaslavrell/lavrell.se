9192da243430417a308c6d0d3ed1de81a971687f
Niklas Lavrell <niklas.lavrell@gmail.com>
2019-02-02 15:52:05 +0800

    layout updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -87,7 +87,7 @@ export const pageQuery = graphql`
     allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       edges {
         node {
-          excerpt
+          excerpt(pruneLength: 165)
           fields {
             slug
           }
